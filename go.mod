ff17f8389eea25b711610475f4afd8a4bd01ba68
Balazs Juhasz <juhaszbal@gmail.com>
2023-09-22 11:04:11 +0200

    Basic fileserver with hit counter & Chi router

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/balazsjuhasz/go-web-server
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func main() {
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	filepathRoot := "."
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	r := chi.NewRouter()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app", fsHandler)
+	r.Handle("/app/*", fsHandler)
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", apiCfg.handlerMetrics)
+	r.Get("/reset", apiCfg.handlerReset)
+
+	corsMux := middlewareCors(r)
+
+	srv := &http.Server{
+		Addr:    ":" + portString,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, portString)
+	log.Fatal(srv.ListenAndServe())
+}
